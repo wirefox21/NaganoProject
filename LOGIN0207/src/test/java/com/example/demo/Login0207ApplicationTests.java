5c81cfdd9501390fe73cbb65ec97115862826589
wirefox21 <wirefox21@gmail.com>
2025-02-05 05:17:18 +0900

    ログイン機能

---
+++ LOGIN0207/src/test/java/com/example/demo/Login0207ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login0207ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
